531b9ed74b31bcce1373da28d111498880632a7a
charliebonza <admin@pelmini.config>
2015-03-16 17:08:43 +0000

    New files for candidate against Ticket 002

---
+++ automation-test/src/main/java/com/codesse/automation/AnotherClass.java
@@ -0,0 +1,13 @@
+package com.codesse.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class AnotherClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Something Else" );
+    }
+}
---
+++ automation-test/src/test/java/com/codesse/automation/AnotherClassTest.java
@@ -0,0 +1,13 @@
+package com.codesse.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class AnotherClassTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Something Else" );
+    }
+}
