94f047323eb62fb171c7a7d5d529155e4b986e6d
charliebonza <charles@bonzagaming.com>
2015-03-17 19:25:14 +0000

    Online edit to check build server

--- automation-test/src/main/java/com/codesse/automation/AnotherClass.java
+++ automation-test/src/main/java/com/codesse/automation/AnotherClass.java
@@ -8,6 +8,6 @@ public class AnotherClass
 {
     public static void main( String[] args )
     {
-        System.out.println( "Something Else" );
+        System.out.println( "Something Else Updated Online" );
     }
 }
