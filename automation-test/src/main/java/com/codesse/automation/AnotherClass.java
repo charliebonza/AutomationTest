d29f9e47322062a62eaa13fccabcd101278ef5cb
charliebonza <charles@bonzagaming.com>
2015-03-18 15:01:30 +0000

    Online update to trigger build

--- automation-test/src/main/java/com/codesse/automation/AnotherClass.java
+++ automation-test/src/main/java/com/codesse/automation/AnotherClass.java
@@ -8,6 +8,6 @@ public class AnotherClass
 {
     public static void main( String[] args )
     {
-        System.out.println( "Something Else Updated Online" );
+        System.out.println( "Something Else Updated Online And Again" );
     }
 }
