e2f06ad18b9f31b70698ca8c4c1716c1a7a4b0b7
charliebonza <charles@bonzagaming.com>
2015-03-19 17:17:02 +0000

    Trigger another build

--- automation-test/src/main/java/com/codesse/automation/AnotherClass.java
+++ automation-test/src/main/java/com/codesse/automation/AnotherClass.java
@@ -8,6 +8,6 @@ public class AnotherClass
 {
     public static void main( String[] args )
     {
-        System.out.println( "Something Else Online Trigger" );
+        System.out.println( "Something Else Another Trigger" );
     }
 }
