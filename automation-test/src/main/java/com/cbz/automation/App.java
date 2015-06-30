9fe7cb530b690c1c6015870761da4655f3948e30
charliebonza <cgirdham@gmail.com>
2015-06-30 13:33:27 +0100

    Changes to the App class

--- automation-test/src/main/java/com/cbz/automation/App.java
+++ automation-test/src/main/java/com/cbz/automation/App.java
@@ -6,8 +6,14 @@ package com.cbz.automation;
  */
 public class App 
 {
+    private static final String SOME_STRING = "test";
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    private void anotherMethod(String stringParameter) {
+        String theString = stringParameter;
     }
 }
